ed28c4329450aa96bdfd973fb6d89f838d46b38e
Víctor Falcón <victoor89@gmail.com>
2024-08-16 11:09:54 +0200

    Change page URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ballink.github.io',
-  base: 'landing',
+  site: 'https://ballink.net',
   integrations: [tailwind(), react()],
 })
